0cd7467adbcf26b66c9a6dd839f6fa65997b124e
allofusbhere <aaron@holland720.com>
2025-08-05 18:34:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,102 +1,57 @@
+
 let currentId = "100000";
 let historyStack = [];
 
-function padId(id) {
-    return id.toString().padEnd(6, '0');
-}
-
 function loadPerson(id) {
-    currentId = padId(id);
-    document.getElementById("mainImage").src = getImageUrl(currentId);
-    document.getElementById("name").innerText = localStorage.getItem(currentId + "_name") || "Name";
-    document.getElementById("dob").innerText = localStorage.getItem(currentId + "_dob") || "DOB";
-    clearGrid();
+  currentId = id;
+  historyStack.push(id);
+  document.getElementById("mainImage").src = `https://cdn.jsdelivr.net/gh/allofusbhere/family-tree-images@main/${id}.jpg`;
+  document.getElementById("personName").innerText = localStorage.getItem(id + "_name") || "SwipeTree";
+  document.getElementById("personDOB").innerText = localStorage.getItem(id + "_dob") || "";
 }
 
-function clearGrid() {
-    document.getElementById("grid").innerHTML = "";
+function goToParent() {
+  let num = parseInt(currentId);
+  let parentId = (Math.floor(num / 10000) * 10000).toString();
+  if (parentId !== currentId) loadPerson(parentId);
 }
 
-function getImageUrl(id) {
-    return `https://cdn.jsdelivr.net/gh/allofusbhere/family-tree-images@main/${id}.jpg`;
+function goToSiblings() {
+  let base = Math.floor(parseInt(currentId) / 10000) * 10000;
+  let siblings = [];
+  for (let i = 1; i <= 9; i++) {
+    let sibId = base + i * 1000;
+    if (sibId !== parseInt(currentId)) siblings.push(sibId.toString());
+  }
+  if (siblings.length) loadPerson(siblings[0]);
 }
 
-function showImages(ids) {
-    clearGrid();
-    const grid = document.getElementById("grid");
-    ids.forEach(id => {
-        const fullId = padId(id);
-        const img = document.createElement("img");
-        img.src = getImageUrl(fullId);
-        img.onerror = () => img.style.display = "none";
-        img.ondblclick = () => editInfo(fullId);
-        img.onclick = () => {
-            historyStack.push(currentId);
-            loadPerson(fullId);
-        };
-        grid.appendChild(img);
-    });
+function goToChildren() {
+  let base = parseInt(currentId);
+  for (let i = 1; i <= 9; i++) {
+    let childId = (base + i * 1000).toString();
+    loadPerson(childId);
+    break;
+  }
 }
 
-function calculateChildrenIds(id) {
-    let base = parseInt(id);
-    let children = [];
-    for (let i = 1; i <= 9; i++) {
-        let childId = base + i * 1000;
-        children.push(childId.toString());
-    }
-    return children;
-}
-
-function calculateSiblingIds(id) {
-    let base = Math.floor(parseInt(id) / 10000) * 10000;
-    let siblings = [];
-    for (let i = 1; i <= 9; i++) {
-        let siblingId = base + i * 1000;
-        if (siblingId.toString() !== id) {
-            siblings.push(siblingId.toString());
-        }
-    }
-    return siblings;
-}
-
-function calculateParentId(id) {
-    return (Math.floor(parseInt(id) / 10000) * 10000).toString();
-}
-
-function showChildren() {
-    const childrenIds = calculateChildrenIds(currentId);
-    showImages(childrenIds);
-}
-
-function showSiblings() {
-    const siblingIds = calculateSiblingIds(currentId);
-    showImages(siblingIds);
-}
-
-function showParent() {
-    const parentId = calculateParentId(currentId);
-    if (parentId !== currentId) {
-        historyStack.push(currentId);
-        loadPerson(parentId);
-    }
+function goToSpouse() {
+  loadPerson(currentId + ".1");
 }
 
 function goBack() {
-    if (historyStack.length > 0) {
-        const prevId = historyStack.pop();
-        loadPerson(prevId);
-    }
+  historyStack.pop();
+  if (historyStack.length > 0) {
+    loadPerson(historyStack.pop());
+  }
 }
 
-function editInfo(id) {
-    const name = prompt("Enter name:", localStorage.getItem(id + "_name") || "");
-    if (name !== null) localStorage.setItem(id + "_name", name);
-
-    const dob = prompt("Enter date of birth:", localStorage.getItem(id + "_dob") || "");
-    if (dob !== null) localStorage.setItem(id + "_dob", dob);
-
-    if (id === currentId) loadPerson(id);
-}
+document.getElementById("mainImage").addEventListener("dblclick", () => {
+  let name = prompt("Enter name:");
+  let dob = prompt("Enter DOB:");
+  if (name) localStorage.setItem(currentId + "_name", name);
+  if (dob) localStorage.setItem(currentId + "_dob", dob);
+  loadPerson(currentId);
+});
 
 window.onload = () => loadPerson(currentId);
