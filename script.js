e522d10311cb118f74da1c4c046ca50cb111c992
allofusbhere <aaron@holland720.com>
2025-08-05 19:08:06 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,104 +1,106 @@
-const anchorImage = document.getElementById("anchor-image");
-const childGrid = document.getElementById("child-grid");
-const siblingGrid = document.getElementById("sibling-grid");
-const backButton = document.getElementById("back-button");
-let currentID = "100000";
-let historyStack = [];
+document.addEventListener("DOMContentLoaded", () => {
+  const anchorImage = document.getElementById("anchor-image");
+  const childGrid = document.getElementById("child-grid");
+  const siblingGrid = document.getElementById("sibling-grid");
+  const backButton = document.getElementById("back-button");
+  let currentID = "100000";
+  let historyStack = [];
 
-function padID(id) {
-  return id.toString().padEnd(6, "0");
-}
+  function padID(id) {
+    return id.toString().padEnd(6, "0");
+  }
 
-function loadImage(id, targetImg) {
-  const baseUrl = "https://allofusbhere.github.io/Family-tree-app/";
-  const tryExtensions = [".jpg", ".JPG"];
+  function loadImage(id, targetImg) {
+    const baseUrl = "https://allofusbhere.github.io/Family-tree-app/";
+    const tryExtensions = [".jpg", ".JPG"];
 
-  function tryNextExtension(index) {
-    if (index >= tryExtensions.length) {
-      targetImg.src = baseUrl + "placeholder.jpg";
-      return;
-    }
-    const url = baseUrl + id + tryExtensions[index];
-    fetch(url, { method: "HEAD" }).then((res) => {
-      if (res.ok) {
-        targetImg.src = url;
-      } else {
-        tryNextExtension(index + 1);
+    function tryNextExtension(index) {
+      if (index >= tryExtensions.length) {
+        targetImg.src = baseUrl + "placeholder.jpg";
+        return;
       }
+      const url = baseUrl + id + tryExtensions[index];
+      fetch(url, { method: "HEAD" }).then((res) => {
+        if (res.ok) {
+          targetImg.src = url;
+        } else {
+          tryNextExtension(index + 1);
+        }
+      });
+    }
+
+    tryNextExtension(0);
+  }
+
+  function loadAnchor(id) {
+    currentID = id;
+    historyStack.push(id);
+    childGrid.innerHTML = "";
+    siblingGrid.innerHTML = "";
+    loadImage(id, anchorImage);
+  }
+
+  function getChildrenIDs(id) {
+    let base = parseInt(id);
+    let place = 1000;
+    let children = [];
+    for (let i = 1; i <= 9; i++) {
+      children.push(padID(base + i * place));
+    }
+    return children;
+  }
+
+  function getSiblingIDs(id) {
+    const num = parseInt(id);
+    const siblingStart = Math.floor(num / 1000) * 1000;
+    let siblings = [];
+    for (let i = 1; i <= 9; i++) {
+      const sibID = siblingStart + i * 1000;
+      if (sibID !== num) {
+        siblings.push(padID(sibID));
+      }
+    }
+    return siblings;
+  }
+
+  function getParentID(id) {
+    return padID(Math.floor(parseInt(id) / 1000) * 1000);
+  }
+
+  function displayGrid(grid, ids) {
+    grid.innerHTML = "";
+    ids.forEach((id) => {
+      const img = document.createElement("img");
+      loadImage(id, img);
+      img.onclick = () => loadAnchor(id);
+      grid.appendChild(img);
     });
   }
 
-  tryNextExtension(0);
-}
+  document.getElementById("children-button").onclick = () => {
+    const children = getChildrenIDs(currentID);
+    displayGrid(childGrid, children);
+  };
 
-function loadAnchor(id) {
-  currentID = id;
-  historyStack.push(id);
-  document.getElementById("child-grid").innerHTML = "";
-  document.getElementById("sibling-grid").innerHTML = "";
-  loadImage(id, anchorImage);
-}
+  document.getElementById("siblings-button").onclick = () => {
+    const siblings = getSiblingIDs(currentID);
+    displayGrid(siblingGrid, siblings);
+  };
 
-function getChildrenIDs(id) {
-  let base = parseInt(id);
-  let place = 1000;
-  let children = [];
-  for (let i = 1; i <= 9; i++) {
-    children.push(padID(base + i * place));
-  }
-  return children;
-}
-
-function getSiblingIDs(id) {
-  const num = parseInt(id);
-  const siblingStart = Math.floor(num / 1000) * 1000;
-  let siblings = [];
-  for (let i = 1; i <= 9; i++) {
-    const sibID = siblingStart + i * 1000;
-    if (sibID !== num) {
-      siblings.push(padID(sibID));
+  document.getElementById("parent-button").onclick = () => {
+    const parent = getParentID(currentID);
+    if (parent !== currentID) {
+      loadAnchor(parent);
     }
-  }
-  return siblings;
-}
+  };
 
-function getParentID(id) {
-  return padID(Math.floor(parseInt(id) / 1000) * 1000);
-}
+  document.getElementById("back-button").onclick = () => {
+    if (historyStack.length > 1) {
+      historyStack.pop();
+      const prev = historyStack.pop();
+      loadAnchor(prev);
+    }
+  };
 
-function displayGrid(grid, ids) {
-  grid.innerHTML = "";
-  ids.forEach((id) => {
-    const img = document.createElement("img");
-    loadImage(id, img);
-    img.onclick = () => loadAnchor(id);
-    grid.appendChild(img);
-  });
-}
-
-document.getElementById("children-button").onclick = () => {
-  const children = getChildrenIDs(currentID);
-  displayGrid(childGrid, children);
-};
-
-document.getElementById("siblings-button").onclick = () => {
-  const siblings = getSiblingIDs(currentID);
-  displayGrid(siblingGrid, siblings);
-};
-
-document.getElementById("parent-button").onclick = () => {
-  const parent = getParentID(currentID);
-  if (parent !== currentID) {
-    loadAnchor(parent);
-  }
-};
-
-document.getElementById("back-button").onclick = () => {
-  if (historyStack.length > 1) {
-    historyStack.pop();
-    const prev = historyStack.pop();
-    loadAnchor(prev);
-  }
-};
-
-loadAnchor(currentID);
+  loadAnchor(currentID);
+});
